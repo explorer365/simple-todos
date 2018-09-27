cae52bac112df3434e9215593305551e56e69ba1
Kayode Daniel <danikay356@gmail.com>
2018-09-27 18:14:05 +0000

    final commit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -7,7 +7,7 @@ import './body.html';
 
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
-  // Meteor.subscribe('tasks');
+  Meteor.subscribe('tasks');
 });
 
 Template.body.helpers({
